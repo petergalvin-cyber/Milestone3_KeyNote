cfe0bd17ba7a59f65d9bfdbf64486e65703501eb
petergalvin-cyber <petergalvin@consultant.com>
2020-02-25 14:06:42 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
